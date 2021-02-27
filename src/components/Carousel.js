f6b545ef313dc5d5eb91c986e85304aa9f07c1f2
Melody Mastache <melodymastache@gmail.com>
2021-02-26 22:54:27 -0500

    changing colours of SVGs -- work paused

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Carousel.css';
-import arrowLeft from '../assets/chevron-left-solid.svg';
-import arrowRight from '../assets/chevron-right-solid.svg';
-import circle from '../assets/circle-solid.svg';
+import { ReactComponent as ArrowLeft } from '../assets/chevron-left-solid.svg';
+import { ReactComponent as ArrowRight } from '../assets/chevron-right-solid.svg';
+import { ReactComponent as Circle } from '../assets/circle-solid.svg';
+import { ReactComponent as Quote } from '../assets/quote.svg';
 
 class Carousel extends React.Component {
     render() {
@@ -10,7 +11,7 @@ class Carousel extends React.Component {
             <div className="carousel-container">
                 <div className="cards">
                     <div className="card left">
-                        <img src="https://placedog.net/250/250" alt="avatar"></img>
+                        <img className="avatars" src="https://placedog.net/250/250" alt="avatar"></img>
                         <div className="text">
                             <h3 className="name">Spot</h3>
                             <i className="position">Hide and Seek Master</i>
@@ -18,19 +19,22 @@ class Carousel extends React.Component {
                         </div>
                     </div>
                     <div className="card active">
-                        <img src="https://placedog.net/250/250?random" alt="avatar"></img>
+                        <img className="avatars" src="https://placedog.net/350/350" alt="avatar"></img>
                         <div className="text">
+                            <Quote className="quotes quote-left" fill="red"></Quote>
                             <h3 className="name">Hammy</h3>
                             <i className="position">Lunchtime Leader</i>
                             <p className="testimony">Woof woof woof woof woof woof woof. Woof woof woof. Woof woof woof woof! Woof. Woof woof woof woof woof woof woof woof. Woof woof. Woof woof woof. Woof woof woof woof woof woof woof woof. Woof Woof woof. Woof woof woof. Woof!</p>
+
                             <div class="arrow-container">
-                                <img src={arrowLeft} alt='arrow-left' className="arrow arrow-left"></img>
-                                <img src={arrowRight} alt="arrow-right" className="arrow arrow-right"></img>
+                                <ArrowLeft className="arrow arrow-left" fill="hsla(316, 0%, 62%, 0.89)"></ArrowLeft>
+                                <ArrowRight className="arrow arrow-right" fill="red"></ArrowRight>
                             </div>
+                            <Quote className="quotes quote-right" fill="grey"> </Quote>
                         </div>
                     </div>
                     <div className="card right">
-                        <img src="https://placedog.net/300/300?random" alt="avatar"></img>
+                        <img className="avatars" src="https://placedog.net/300/300" alt="avatar"></img>
                         <div className="text">
                             <h3 className="name">Mario</h3>
                             <i className="position">Professional Hero</i>
@@ -39,9 +43,7 @@ class Carousel extends React.Component {
                     </div>
                 </div>
                 <div className="tracker">
-                    <img src={circle} alt="circle"></img>
-                    <img src={circle} alt="circle"></img>
-                    <img src={circle} alt="circle"></img>
+                    <Circle fill="red"></Circle>
                 </div>
             </div>
         )
